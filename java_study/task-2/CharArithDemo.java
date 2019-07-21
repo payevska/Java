de4e6b60c40a979779567d51703e5d95c669b8ea
Yuliya Payevska <y.payevska@gmail.com>
2019-07-21 15:50:04 +0300

    task-2 variable char

---
+++ java_study/task-2/CharArithDemo.java
@@ -0,0 +1,14 @@
+class CharArithDemo {
+	public static void main(String args[]) {
+		char ch;
+
+		ch = 'X';
+		System.out.println("ch содержит " + ch);
+
+		ch++; // инкрементировать переменную ch, 'Y'
+		System.out.println("теперь ch содержит " + ch);
+
+		ch = 90; // присвоить переменной ch значение 'Z'
+		 System.out.println("теперь ch содержит " + ch);
+	}
+}

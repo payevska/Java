f71282553c52cb32815be2430d5f0162dc276bdd
Yuliya Payevska <y.payevska@gmail.com>
2019-07-21 15:18:58 +0300

    task-1 variable double

---
+++ java_study/Hypot.java
@@ -0,0 +1,14 @@
+/*
+Определение длины гипотенузы исходя из длины катетов, 
+по теореме Пифагора
+*/
+class Hypot {
+	public static void main(String args[]) { 
+		double x, y, z;
+		x = 3;
+		y = 4;
+		
+		z = Math.sqrt(x*x + y*y);
+		System.out.println("Длина гипотенузы:  " + z);
+	}
+}

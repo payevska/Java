82a55d97770f249f15552fbc855623f4648a1c5a
Yuliya Payevska <y.payevska@gmail.com>
2019-08-02 20:18:55 +0300

    task-18 shorthand assignments

---
+++ java_study/task- 18/OpEquals.java
@@ -0,0 +1,15 @@
+// shorthand assignments
+class OpEquals {
+	public static void main(String args[]) {
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		a += 5;
+		b *= 4;
+		c += a * b;
+		c %= 6;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+	}
+}

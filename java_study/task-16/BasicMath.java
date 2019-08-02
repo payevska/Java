8770d8252ac7710cc7a557cbaaa981210539489b
Yuliya Payevska <y.payevska@gmail.com>
2019-08-02 17:43:23 +0300

    task-16 arithmetic operation with int and double values

---
+++ java_study/task-16/BasicMath.java
@@ -0,0 +1,30 @@
+// Demonstration basic mathematic operation
+class BasicMath {
+	public static void main(String args[]) {
+		//arithmetic operation with int value
+		System.out.println("Arithmetic operation with int value");
+		int a = 1 + 1;
+		int b = a * 3;
+		int c = b / 4;
+		int d = c - a;
+		int e = -d;
+		System.out.println("a = " + a);
+		System.out.println("a = " + b);
+		System.out.println("a = " + c);
+		System.out.println("a = " + d);
+		System.out.println("a = " + e);
+
+		//arithmetic operation with double value
+		System.out.println("\nArithmetic operation with double value");
+		double da = 1 + 1;
+		double db = da * 3;
+		double dc = db / 4;
+		double dd = dc - a;
+		double de = -dd;
+		System.out.println("a = " + da);
+		System.out.println("a = " + db);
+		System.out.println("a = " + dc);
+		System.out.println("a = " + dd);
+		System.out.println("a = " + de);
+	}
+}

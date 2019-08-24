adf9bb87da33079e401d09dbb95a59d68d531a61
Yuliya Payevska <y.payevska@gmail.com>
2019-08-24 19:24:48 +0300

    task-methodsParams methods and params

---
+++ javarush/task-metodsParams/BridgeOfficer.java
@@ -0,0 +1,37 @@
+public class BridgeOfficer {
+
+	int maxWeight;
+
+	public BridgeOfficer(int normalVolume) {
+		this.maxWeight = normalVolume;
+	}
+
+	public boolean checkTruck(Truck truck) {
+		if (truck.weight > maxWeight) {
+			return false;		
+		} else {
+			return true;
+		}
+	}
+
+	public static void main(String[] args) {
+		Truck first = new Truck();
+		first.weight = 10000;
+		Truck second = new Truck();
+		second.weight = 20000;
+
+		BridgeOfficer officer = new BridgeOfficer(15000);
+		System.out.println("Truck number 1! Can I go, officer?");
+		//call method checkTruck
+		boolean canFirstTruckGo = officer.checkTruck(first); //true
+		System.out.println(canFirstTruckGo);
+
+		System.out.println();
+
+		System.out.println("Truck number 2! Can I go too?");
+		//call method checkTruck
+		boolean canSecondTruckGo = officer.checkTruck(second); //false
+		System.out.println(canSecondTruckGo);
+
+	}
+}
